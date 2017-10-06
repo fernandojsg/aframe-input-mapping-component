85f1a1c25f059d6df1961d14b898359dfbfc25c7
Fernando Serrano <fernandojsg@gmail.com>
2017-10-06 19:52:04 +0200

    Bump dist

--- dist/aframe-input-mapping-component.js
+++ dist/aframe-input-mapping-component.js
@@ -56,7 +56,8 @@
 	AFRAME.registerSystem('input-mapping', {
 	  schema: {},
 	  mappings: {},
-	  currentSection: 'DEFAULT',
+	  mappingsPerControllers: {},
+	  currentMapping: 'default',
 
 	  /**
 	   * Set if component needs multiple instancing.
@@ -67,47 +68,78 @@
 	   * Called once when component is attached. Generally for initial setup.
 	   */
 	  init: function () {
-
 	    if (AFRAME.DEFAULT_INPUT_MAPPINGS) {
-	      this.registerInputMappings(AFRAME.DEFAULT_INPUT_MAPPINGS);
+	      this.registerMappings(AFRAME.DEFAULT_INPUT_MAPPINGS);
 	    }
 
 	    var self = this;
 
 	    // Controllers
-	    this.sceneEl.addEventListener('controller-connected', function (evt) {
-	      var controllerModel = evt.detail.type;
-	      if (!this.mappings) {
+	    this.sceneEl.addEventListener('controllerconnected', function (evt) {
+	      if (!self.mappings) {
 	        console.warn('controller-mapping: No mappings defined');
 	        return;
 	      }
 
-	      var controllerMappings = this.mappings[controllerModel];
-	      if (!controllerMappings) {
-	        console.warn('controller-mapping: No mappings defined for controller type: ', controllerModel);
-	        return;
+	      if (!self.mappingsPerControllers[controllerModel]) {
+	        self.mappingsPerControllers[controllerModel] = {};
 	      }
 
-	      for (var eventName in controllerMappings) {
-	        self.sceneEl.addEventListener(controllerMappings[eventName], function(evt) {
-	          console.log(eventName, controllerMappings[eventName], evt);
-	          self.sceneEl.emit(eventName, evt);
+	      var mappingsPerController = self.mappingsPerControllers[controllerModel];
+
+	      for (var mappingName in self.mappings) {
+	        var mapping = self.mappings[mappingName];
+	        var controllerModel = evt.detail.name;
+
+	        var controllerMappings = mapping[controllerModel];
+	        if (!controllerMappings) {
+	          console.warn('controller-mapping: No mappings defined for controller type: ', controllerModel);
+	          return;
+	        }
+
+	        for (var eventName in controllerMappings) {
+	          var mapping = controllerMappings[eventName];
+	          if (!mappingsPerController[eventName]) {
+	            mappingsPerController[eventName] = {};
+	          }
+
+	          mappingsPerController[eventName][mappingName] = mapping;
+	        }
+	      }
+
+	      for (var eventName in mappingsPerController) {
+	        self.sceneEl.addEventListener(eventName, function(event2) {
+	          var mapping = mappingsPerController[event2.type];
+
+	          var mappedEvent = mapping[self.currentMapping] ? mapping[self.currentMapping] : mapping.default;
+	          if (mappedEvent) {
+	            evt.detail.target.emit(mappedEvent, event2);
+	          }
 	        });
 	      }
 	    });
 
-	    // Keyboard
-	    if (this.mappings['KEYBOARD']) {
-	      document.addEventListener('keyup', function (event) {
+	    // Keyboard (Very WIP)
+	    var self = this;
+	    var scene = this.sceneEl;
+	    document.addEventListener('keyup', function (event) {
+	      var mappings = self.mappings[self.currentMapping];
 
-	      });
-	      document.addEventListener('keydown', function (event) {
+	      if (mappings && mappings.keyboard) {
+	        mappings = mappings.keyboard;
 
-	      });
-	      document.addEventListener('keypress', function (event) {
-
-	      });
-	    }
+	        var mapEvent = mappings[event.key + '_up'];
+	        if (mapEvent) {
+	          scene.emit(mapEvent);
+	        }
+	      }
+	    });
+	    /*
+	    document.addEventListener('keydown', function (event) {
+	    });
+	    document.addEventListener('keypress', function (event) {
+	    });
+	    */
 	  },
 
 	  /**
@@ -139,31 +171,28 @@
 	   */
 	  play: function () { },
 
-	  registerInputMappings: function (mappings) {
+	  registerMappings: function (mappings) {
+	    // @todo Overwrite just the conflicts instead of the whole mapping
 	    this.mappings = mappings;
 	  },
 
-	  setActiveSection: function (section) {
-	    if (this.mappings[section]) {
-	      this.currentSection = section;
+	  getActiveMapping: function () {
+	    return this.currentMapping;
+	  },
+
+	  setActiveMapping: function (mapping) {
+	    if (this.mappings[mapping]) {
+	      this.currentMapping = mapping;
 	    } else {
-	      console.warn('aframe-input-mapping-component: Trying to activate a section that doesn\'t exist:', section);
+	      console.warn('aframe-input-mapping-component: Trying to activate a mapping that doesn\'t exist:', mapping);
 	    }
 	  }
 	});
 
-	AFRAME.registerInputMappings = function(mappings) {
+	AFRAME.registerMappings = function(mappings) {
 	  // Add mapping
-	  AFRAME.scenes[0].sceneEl.systems['input-mapping'].registerInputMappings(mappings);
+	  AFRAME.scenes[0].sceneEl.systems['input-mapping'].registerMappings(mappings);
 	};
-
-	AFRAME.DEFAULT_INPUT_MAPPINGS = {
-	  'GLOBAL': {
-	    'KEYBOARD': {
-	      testlog: 't'
-	    }
-	  }
-	}
 
 
 /***/ })
